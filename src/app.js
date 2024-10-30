9ec4269c65159df89aa0d5f18de1eb979a01a3be
ajmal31 <ajmalmuhammed846@gmail.com>
2024-10-31 01:26:31 +0530

    Route handlers :playing with code deep level understandin

--- src/app.js
+++ src/app.js
@@ -1,13 +1,32 @@
-const express=require("express")
+const express = require("express");
 
+const app = express();
 
-const app=express()
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("first");
+    next();
+  },
+  (req, res,next) => {
+    console.log("second");
+    // res.send("second");
+    next()
+  },
+  (req, res,next) => {
+    console.log("third");
+    // res.send("second");
+    next()
+  },
+  (req, res,next) => {
+    console.log("fourth");
+    // res.send("second");
+    next()
+  },(req,res)=>{
+    res.send("6th")
+  }
+);
 
-app.use("/",(req,res)=>{
-  res.send("Welcome To Dev Tinder")
-})
-
-app.listen(3000,()=>{
-
-console.log("Server is listening on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
